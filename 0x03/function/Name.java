ff6ad1cd3765d73bae05e0f5ca795c667ec1dfdd
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-04-08 10:49:29 -0300

    task 1

---
+++ 0x03/function/Name.java
@@ -0,0 +1,36 @@
+public class Functions {
+  
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.save(user);
+    }
+   
+    public void saveUser(String userName, User user, Date date, boolean isAdmin){}; 
+
+    
+    public User saveUser(User user){
+      if(user.isAdmin){
+      user.setRole(user);
+    }
+      return userRepository.save(user);
+    }
+
+    
+    public Environment getEnvironment(){
+        return environmentService.getEnvironment(getVersion());
+    }
+    public Release getRelease(){
+        return releaseService.getRelease(getVersion());
+    }
+    private String getVersion(){ 
+        return versionService.getVersion();
+    }
+
+}
