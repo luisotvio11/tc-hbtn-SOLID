5025f066f553abc6bdffc335bb599c1c0ba15c0f
luisotv <luisotavio@ciandt.com>
2022-04-11 13:24:13 -0300

    task 1

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,6 +1,4 @@
-public class Comments {
-
-    // Copyright (C) 2022 by CleanCode.
+// Copyright (C) 2022 by CleanCode.
     // Released under the terms of the GNU General Public License version 2 or later.
 
 
@@ -12,9 +10,6 @@ public class Comments {
     public Hours getHoursInQuarter(){}; 
 
 
-    //public void checkXMLParser(){
-    //   // some business logic here
-    //}
 
     // TODO Este métod deve ser removido
     public void check(){
@@ -22,9 +17,16 @@ public class Comments {
     }
 
 
-  
+    
+
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
     public List<Result> publishResults(Date date) {
         // some business logic here
     }
 }
-
