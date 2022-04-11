cb1c038c2b8e293fe8ba44f97f9f54ff70a8b647
luisotv <luisotavio@ciandt.com>
2022-04-11 13:45:00 -0300

    task 2

---
+++ 0x04/User.java
@@ -0,0 +1,17 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+    int IdUser;
+    
+    String Descricao;
+    
+    final static int IDADEMINIMA = 18;
+    
+  
+    // função que coleta todos os users
+    
+    public AllUsers() {return [];}
+    
+    
+    }
---
+++ 0x04/convencao/User.java
@@ -0,0 +1,16 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+    int IdUser;
+    
+    String Descricao;
+    
+  
+    // função que coleta todos os users
+    
+    public AllUsers() {return [];}
+    
+ 
+    
+    }
