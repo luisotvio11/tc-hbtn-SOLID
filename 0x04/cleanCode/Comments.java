1673d5deb5062c0d4f9b6850d16bd89b272b92b4
luisotv <luisotavio@ciandt.com>
2022-04-11 13:22:10 -0300

    task 0

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,30 @@
+public class Comments {
+
+    // Copyright (C) 2022 by CleanCode.
+    // Released under the terms of the GNU General Public License version 2 or later.
+
+
+    // this method delivers owner of testcase
+    public Owner getTestcaseOwner(Testcase testcase){}; 
+
+
+    // returns hours in month
+    public Hours getHoursInQuarter(){}; 
+
+
+    //public void checkXMLParser(){
+    //   // some business logic here
+    //}
+
+    // TODO Este métod deve ser removido
+    public void check(){
+        return;
+    }
+
+
+  
+    public List<Result> publishResults(Date date) {
+        // some business logic here
+    }
+}
+
